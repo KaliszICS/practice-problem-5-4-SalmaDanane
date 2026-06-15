afc1b0316de9905d3d6825cda501411989055ca8
SalmaDanane <343249595@tdsb.ca>
2026-06-15 08:42:28 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,67 @@
+/*
+
+	Author
+	Date created
+	Date last modified
+	File name - Arrays, split, swapping
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-}
+	public static void stringSwap(String[] arr, int num1, int num2) {
+		
+		String temp;
+		temp = arr[num1];
+		
+		arr[num1] = arr[num2];
+		arr[num2] = temp;
+
+	}
+
+	public static void reverse(int[] arr) {
+		
+		int[] temp = new int[arr.length];
+		int count = arr.length - 1;
+		
+		for (int i = 0; i < arr.length; i++) {
+			
+			temp[i] = arr[count];
+			count--;
+		}
+		
+		for (int i2 = 0; i2 < arr.length; i2++) {
+			 arr[i2] = temp[i2]; 
+		}
+	}
+
+	public static String[] toWordArray(String word) {
+		
+		String[] arr = word.split(" ");
+		int count = 0;
+		int index = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (!arr[i].equals("")) {
+				count++;
+			}
+		}
+		
+		String[] arr2 = new String[count];
+		
+		for (int i2 = 0; i2 < arr.length; i2++) {
+			
+			if (!arr[i2].equals("")) {
+				
+				arr2[index] = arr[i2];
+				index++;
+			}
+		}
+		
+		return arr2;
+		
+		}
+	}
